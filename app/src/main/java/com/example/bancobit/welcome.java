14c3ee628a00247b21357a55b791e0e5b0edb30a
AxelT2 <telloatondo@yahoo.com.mx>
2021-10-21 19:02:19 -0600

    20/10/2021

---
+++ app/src/main/java/com/example/bancobit/welcome.java
@@ -0,0 +1,24 @@
+package com.example.bancobit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Shader;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
+
+public class welcome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+
+
+}
